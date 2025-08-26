fb7219ad0e64e1272d3564091674c41985171adc
turgaymammadov <memedovturqay871@gmail.com>
2025-08-26 12:46:49 +0400

    updated

---
+++ client/src/app/note.tsx
@@ -0,0 +1,122 @@
+'use client';
+import React, { useRef, useState } from "react";
+// import EditIcon from '@mui/icons-material/Edit'; 
+// import DeleteIcon from '@mui/icons-material/Delete'; 
+// import SaveIcon from '@mui/icons-material/Save'; 
+import axios from "axios"
+export type NoteType={
+  _id:number
+  title:string,
+  description:string,
+  createdAt:string
+
+}
+
+type NoteProps={
+  note:string,
+  setNote:React.Dispatch<React.SetStateAction<string>>,
+  setTitle:React.Dispatch<React.SetStateAction<string>>,
+  notes:NoteType[],
+  setNotes:React.Dispatch<React.SetStateAction<NoteType[]>>
+  title:string
+}
+const Note=({note, title, setNote, setTitle, notes,  setNotes}: NoteProps)=>{
+
+    const [edited, setEdited] = useState<number | null>(null);
+    const [readOnly, setReadOnly]=useState(true)
+    const inputRef=useRef<HTMLInputElement | null>(null);
+    const textareaRef=useRef<HTMLTextAreaElement | null>(null);
+
+
+    const handleDelete=async (id:number)=>{
+      console.log(id)
+      await axios.delete(`http://localhost:5000/api/notes/${id}`, {
+        headers:{
+          'Content-Type':"application/json"
+        }
+      })
+        setNotes(notes.filter((note:NoteType)=>note._id!==id))
+ }
+
+ const handleEdit=async(id:number, title:string, description:string)=>{
+      setReadOnly(false)
+    setEdited(id)
+    setTitle(title)
+    setNote(description)
+    setTimeout(()=>{
+    textareaRef.current?.focus()
+       })
+ }
+ 
+ const handleSave=async(id:number, title:string, description:string,)=>{
+    await axios.put(`http://localhost:5000/api/notes/${id}`,{
+    title,
+    description
+  },
+{
+  headers:{
+    "Content-Type":"application/json",  
+  }
+}
+)
+   setReadOnly(true)
+      setNotes(
+      notes.map((item:NoteType)=>item._id===id ? { ...item, title,description}   : item ))
+      setEdited(null)
+      setTitle('')
+      setNote('')
+     
+     
+ }
+
+    return(
+         <div className="flex justify-center gap-4 flex-wrap pt-4 mt-4">
+        { notes.map((item:NoteType)=>{
+            return (
+                <div className="card flex flex-col flex-wrap rounded shadow-lg py-2 px-4 gap-2" key={item._id}>
+        <div className="flex ">
+          <div>
+          <label htmlFor="title"></label>
+            <input className="font-bold pt-2 text-xl"  type="text"  ref={edited===item._id ? inputRef : null} defaultValue={item.title}  readOnly={readOnly}   onChange={(e)=>setTitle(e.target.value)} placeholder="title"/>
+            </div>
+  
+          <div className="dropdown">
+            <button className="dropdown-btn">...</button>
+            <div className="dropdown-menu">
+               { 
+        edited===item._id ?  <button  onClick={()=>handleSave(edited,title,note
+          
+          )}>
+Save
+        </button> :
+  <button onClick={()=>handleEdit(item._id, item.title, item.description)}> Edit</button>
+      }
+         
+        <button onClick={()=>handleDelete(item._id)}> Delete </button>
+           
+          </div>
+            </div>
+         
+        </div>
+         <label htmlFor="desc"></label>
+            <textarea id='desc'  ref={edited===item._id ? textareaRef : null}  defaultValue={item.description} readOnly={edited!==item._id} onChange={(e)=>setNote(e.target.value)} placeholder="note"/>
+        <div className="flex justify-between items-center mt-8">
+        <div><p>
+          {new Date(item.createdAt).toLocaleDateString('en-Us', {
+            year:'numeric',
+            month:"2-digit",
+            day:"2-digit"
+          })}
+          </p></div>
+      
+
+        </div>
+        </div>
+            )
+          })
+        }
+        </div>
+
+    )
+}
+export default Note;
---
+++ client/src/app/notes.tsx
@@ -0,0 +1,107 @@
+'use client';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import React from "react";
+import {Modal, ModalBody, ModalContent, ModalHeader, useDisclosure} from "@nextui-org/modal"
+import AddIcon from '@mui/icons-material/Add';
+import Note from "./note";
+export type NoteType={
+  _id:number
+  title:string,
+  description:string,
+  createdAt:string
+
+}
+const Notes=()=>{
+    const [title, setTitle]=useState<string>("") || null
+    const [note, setNote]=useState<string>("") || null
+    const {isOpen, onOpen, onOpenChange, onClose}=useDisclosure()
+    const [notes, setNotes]=useState<NoteType[]>([]);
+
+  useEffect(()=>{
+    // localStorage.removeItem('notes')
+    const data=localStorage.getItem('notes');
+    if(data){
+        const response= JSON.parse(data) 
+        setNotes(response)
+    }   
+}, [])
+    useEffect(()=>{
+        const getNotes=async()=>{
+          const results=await axios.get('http://localhost:5000/api/')
+        setNotes(results.data)
+        }
+        getNotes()
+      
+        localStorage.setItem('notes',JSON.stringify(notes))
+       },[notes])
+
+    const handleForm=async (e:React.FormEvent)=>{
+        e.preventDefault();
+        
+        try{
+const results= await axios.post('http://localhost:5000/api/notes/', {title:title, description:note},  {
+    headers:{
+        'Content-Type':'application/json',
+    }
+})
+    if(title.trim()){    
+            setNotes([...notes, results.data])      
+            setTitle('')
+            setNote('')
+            onClose()
+        }
+        }
+        catch(err){
+           console.log(err)
+        }
+    }
+    return(
+        <div>
+                  <div className="text-center add">
+                   <button type="submit" className="outline-none text-white" onClick={onOpen} aria-label="add">
+                    <AddIcon/>
+                   </button>
+                   </div>
+             
+        <Note  title={title} setTitle={setTitle} note={note} notes={notes} setNotes={setNotes} setNote={setNote}/>
+
+        <div className="pt-8 text-center flex justify-center align-center" >
+            
+             <Modal isOpen={isOpen} onOpenChange={onOpenChange}>                    
+                    <ModalContent>                      
+                                   <ModalHeader>
+                            Create Note
+                        </ModalHeader>
+                    <ModalBody>
+
+                    <form  onSubmit={handleForm} className="flex flex-col gap-4  ">
+                            <label>
+                              <input type="text" className="w-full outline-none rounded bg-gray-100 py-2 px-3" placeholder="Title"  value={title} onChange={(e)=>setTitle(e.target.value)}></input>
+                            </label>
+                            <label>
+                                <textarea cols={9} rows={5} placeholder="Take a note"  className="bg-gray-100 outline-none rounded px-4 py-2 w-full" value={note} onChange={(e)=>setNote(e.target.value)}/>
+                            </label>
+                            <div className="text-center pb-5"> 
+                                 <button type="submit" className="bg-blue-500 rounded outline-none py-2 px-8 text-white" aria-label="submit">Add</button>
+                                 </div>
+                           </form>
+
+                    </ModalBody>
+                         
+                         
+                 
+                    {/* <ModalFooter>
+                    <button className="" onClick={()=>setOpen(false)}>Close</button>
+                    </ModalFooter> */}
+                    </ModalContent>
+                    </Modal>
+
+        </div>
+                
+
+</div>     
+     
+    )
+}
+export default Notes;
